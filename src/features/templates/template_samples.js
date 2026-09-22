57d795124e7c77c893be126bdfc768c0f56a3a85
Mahendra Wira Dharma <mwdhrmaaa@users.noreply.github.com>
2026-09-22 21:50:14 +0800

    feat(samples): synthesize authentic user photography preview and document japan_travel_diary template

--- src/features/templates/template_samples.js
+++ src/features/templates/template_samples.js
@@ -320,5 +320,17 @@ export const TEMPLATE_SAMPLES = {
     caption: 'Life is made up of small joys: nice food,',
     subtitle: 'gentle breeze, lazy afternoons and peaceful nights.',
     date: 'JOURNAL // 2026'
+  },
+  japan_travel_diary: {
+    src: U.p0,
+    photos: [
+      U.p0, U.p1, U.p2,
+      U.p3, U.p4, U.p5,
+      U.p6, U.p7, U.p8,
+      U.p9, U.p10, U.p11
+    ],
+    caption: 'JAPAN TRAVEL DIARY',
+    subtitle: 'ヒッチハイクー！ // ぼちぼち帰ろっか // see you !',
+    date: '35MM FILM // 2026'
   }
 };
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'framera-cache-v16';
+const CACHE_NAME = 'framera-cache-v17';
 const FONT_CACHE_NAME = 'framera-fonts-v1';
 
 const STATIC_ASSETS = [
@@ -54,6 +54,7 @@ const STATIC_ASSETS = [
   './assets/silver_gelatin_duo_preview.png',
   './assets/metropolis_story_preview.png',
   './assets/binder_clip_duo_preview.png',
+  './assets/japan_travel_preview.png',
   './assets/user_provided/1790039442490_0_scaled_1000000128.jpg',
   './assets/user_provided/1790039442536_1_scaled_1000000129.jpg',
   './assets/user_provided/1790039442582_2_scaled_1000000126.jpg',
