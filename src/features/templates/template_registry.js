540d006047a9d4a26fdab4909bf09636dcd2a559
Mahendra Wira Dharma <mwdhrmaaa@users.noreply.github.com>
2026-09-23 08:10:54 +0800

    refactor(template): remove Japan Travel Film Diary template and category

--- src/features/templates/template_registry.js
+++ src/features/templates/template_registry.js
@@ -42,7 +42,6 @@ import { seasideDiptychTemplate } from './seaside_diptych_template.js';
 import { silverGelatinTemplate } from './silver_gelatin_template.js';
 import { metropolisStoryTemplate } from './metropolis_story_template.js';
 import { binderClipDuoTemplate } from './binder_clip_duo_template.js';
-import { japanTravelDiaryTemplate } from './japan_travel_diary_template.js';
 import { redCookedTemplate } from './red_cooked_template.js';
 
 export const TEMPLATE_REGISTRY = {};
@@ -131,7 +130,6 @@ export function initDefaultTemplates() {
   registerTemplate(silverGelatinTemplate);
   registerTemplate(metropolisStoryTemplate);
   registerTemplate(binderClipDuoTemplate);
-  registerTemplate(japanTravelDiaryTemplate);
   registerTemplate(redCookedTemplate);
 }
 
