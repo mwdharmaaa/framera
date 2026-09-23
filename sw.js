a8d34b726d1b4799c24759df0023a6717ca6e79e
Mahendra Wira Dharma <mwdhrmaaa@users.noreply.github.com>
2026-09-23 08:09:08 +0800

    perf(sw): precache red_cooked preview and bump cache version to v18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'framera-cache-v17';
+const CACHE_NAME = 'framera-cache-v18';
 const FONT_CACHE_NAME = 'framera-fonts-v1';
 
 const STATIC_ASSETS = [
@@ -55,6 +55,7 @@ const STATIC_ASSETS = [
   './assets/metropolis_story_preview.png',
   './assets/binder_clip_duo_preview.png',
   './assets/japan_travel_preview.png',
+  './assets/red_cooked_preview.png',
   './assets/user_provided/1790039442490_0_scaled_1000000128.jpg',
   './assets/user_provided/1790039442536_1_scaled_1000000129.jpg',
   './assets/user_provided/1790039442582_2_scaled_1000000126.jpg',
